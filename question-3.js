4c46729b7a4502806ee8d6421003392ceeb9635f
mohamednageh7 <50457023+mohamednageh7@users.noreply.github.com>
2020-10-28 09:01:55 +0300

    Update question-3.js
    increase performance

--- question-3.js
+++ question-3.js
@@ -1,11 +1,8 @@
 let arr = [1, 2, 2, 3, 4, 4, 5];
 
-let count = arr.length - 1;
 let uniqe = new Set();
-while (count >= 0) {
   arr.forEach((item) => {
     uniqe.add(item);
   });
-  count--;
-}
+
 console.log(uniqe);
